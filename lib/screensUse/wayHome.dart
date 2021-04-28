7eb4da4522ef47d2ac772a2eeb9dd02f2ef692c7
TatponG44299 <tatpongrattanavichai@gmail.com>
2021-04-28 16:34:26 +0700

    button ระยะทาง

--- lib/screensUse/wayHome.dart
+++ lib/screensUse/wayHome.dart
@@ -20,59 +20,64 @@ class _SelectWayState extends State<SelectWay> {
         child: Container(
           child: Stack(
             children: <Widget>[
+              
               //trRangeSlider()
-              SleekCircularSlider(
-                initialValue: 10,
-                min: 0,
-                max: 51,
-                appearance: CircularSliderAppearance(
-                  size: 250,
-                  customColors: CustomSliderColors(
-                    hideShadow: true,
-                    //shadowColor: Colors.black,
-                    dotColor: Colors.red[700],
-                    progressBarColors: [Colors.red, Colors.red[700]],
-                    progressBarColor: Colors.red[700],
-                    trackColor: Colors.grey[700],
-                  ),
-                  customWidths: CustomSliderWidths(
-                    trackWidth: 8,
-                    progressBarWidth: 10,
-                    //shadowWidth: 20,
-                    handlerSize: 10,
-                  ),
-                  angleRange: 360,
-                  startAngle: 270,
-                ),
-                onChange: (v) {
-                  setState(() {
-                    km = (v).floor();
-                  });
-                },
-                innerWidget: (v) {
-                  return Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Text('ระยะทาง'),
-                        Text(
-                          '$km',
-                          style: TextStyle(
-                              fontSize: 54, fontWeight: FontWeight.bold),
-                        ),
-                        Text('กิโลเมตร(กม.)')
-                      ],
+              setDis(),
+              Container(
+                margin: EdgeInsets.only(top: 50),
+                child: SleekCircularSlider(
+                  initialValue: 10,
+                  min: 0,
+                  max: 51,
+                  appearance: CircularSliderAppearance(
+                    size: 250,
+                    customColors: CustomSliderColors(
+                      hideShadow: true,
+                      //shadowColor: Colors.black,
+                      dotColor: Colors.red[700],
+                      progressBarColors: [Colors.red, Colors.red[700]],
+                      progressBarColor: Colors.red[700],
+                      trackColor: Colors.grey[700],
                     ),
-                  );
-                },
-              )
+                    customWidths: CustomSliderWidths(
+                      trackWidth: 8,
+                      progressBarWidth: 10,
+                      //shadowWidth: 20,
+                      handlerSize: 10,
+                    ),
+                    angleRange: 360,
+                    startAngle: 270,
+                  ),
+                  onChange: (v) {
+                    setState(() {
+                      km = (v).floor();
+                    });
+                  },
+                  innerWidget: (v) {
+                    return Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text('ระยะทาง'),
+                          Text(
+                            '$km',
+                            style: TextStyle(
+                                fontSize: 54, fontWeight: FontWeight.bold),
+                          ),
+                          Text('กิโลเมตร(กม.)')
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+              cilckButton(),
             ],
           ),
         ),
       ),
     );
-    
   }
 
   // RangeSlider trRangeSlider() {
@@ -92,4 +97,36 @@ class _SelectWayState extends State<SelectWay> {
   // },
   //         );
   // }
+  // 
+  Widget setDis() {
+    return Container(
+      margin: EdgeInsets.only(left: 30),
+      child: Text(
+        'กำหนดระยะทาง',
+        style: TextStyle(fontSize: 28.0, color: Colors.redAccent[700]),
+      ),
+    );
+  }
+
+  Widget cilckButton() => Container(
+    margin: EdgeInsets.only(top: 350) ,
+      width: 250.0,
+      child: ClipRRect(
+        //ลดเหลี่ยมปุ่ม
+        borderRadius: BorderRadius.circular(10),
+        child: FlatButton(
+          padding: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+          color: Colors.redAccent[700],
+          onPressed: () {
+            if (km != null) ;
+            Text('km');
+            print(km);
+          },
+          child: Text(
+            'ค้นหา',
+            style: TextStyle(color: Colors.white, fontSize: 18.0),
+          ),
+        ),
+      ));
+      
 }
